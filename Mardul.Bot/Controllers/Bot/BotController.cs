04b074a4bc7ed523c886b5f8f6e7b08bff2001ee
Karpunin Dmitry <kotel77@inbox.ru>
2023-03-02 12:15:49 +0300

    changing botController

--- Mardul.Bot/Controllers/Bot/BotController.cs
+++ Mardul.Bot/Controllers/Bot/BotController.cs
@@ -1,4 +1,5 @@
 ﻿using Mardul.Bot.Services.BotService;
+using Mardul.Bot.Services.CommandService;
 using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -9,18 +10,27 @@ namespace Mardul.Bot.Controllers.Bot
     [ApiController]
     public class BotController : ControllerBase
     {
-        private readonly TelegramBotClient _botClient;
-
-        public BotController(BotService botService)
+        //private readonly TelegramBotClient _botClient;
+        private readonly ICommandService _commandService;
+        public BotController(ICommandService commandService)
         {
-            _botClient = botService.GetBotAsync().Result;
+           _commandService = commandService;
         }
+        //[Route("1ZwWxG8wNBvLvUlncprlZG3l9q1RdseE")]
+        //[HttpPost]
+        //public async Task<IActionResult> UpdateMessage([FromBody] Update update)
+        //{
+        //    if (update == null) return Ok("нет сообщений");
+        //    await _botClient.SendTextMessageAsync(update.Message.Chat.Id, $"простите, не понимаю :(");
+
+        //    return Ok();
+        //}
+
         [Route("1ZwWxG8wNBvLvUlncprlZG3l9q1RdseE")]
         [HttpPost]
         public async Task<IActionResult> UpdateMessage([FromBody] Update update)
         {
-            if (update == null) return Ok("нет сообщений");
-            await _botClient.SendTextMessageAsync(update.Message.Chat.Id, $"простите, не понимаю :(");
+            await _commandService.ExecuteAsync(update);
 
             return Ok();
         }
