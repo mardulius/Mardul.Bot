a020121b1e463cc53de04ace8bfbee9842eb156b
Dmitry Karpunin <kotel77@inbox.ru>
2023-03-03 11:19:39 +0300

    delete unuse code

--- Mardul.Bot/Services/BotService/BotSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mardul.Bot.Services.BotService
-{
-    public static class BotSettings
-    {
-        public const string APIKEY = "1263428243:AAHDh7kCBRkyDKD11U6rTVIM6Zh7WDPrFu4";
-        public const string APIURL = @"https://a15471-2823.s.d-f.pw/api/bot/1ZwWxG8wNBvLvUlncprlZG3l9q1RdseE";
-    }
-}
