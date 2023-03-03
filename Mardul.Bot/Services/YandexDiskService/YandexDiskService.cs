52a259723bb6d71cd75ce2e41dcf107415a27878
Dmitry Karpunin <kotel77@inbox.ru>
2023-03-03 15:16:07 +0300

    fix

--- Mardul.Bot/Services/YandexDiskService/YandexDiskService.cs
+++ Mardul.Bot/Services/YandexDiskService/YandexDiskService.cs
@@ -8,18 +8,11 @@
         {
             _httpClientFactory = httpClientFactory;
         }
-        public async Task<bool> AuthorizeAsync()
+        
+
+        public Task<bool> CreateFolderAsync()
         {
-            var httpRequestMessage = new HttpRequestMessage(
-            HttpMethod.Get,
-            "https://oauth.yandex.ru/authorize?response_type=code&client_id=e0f4b3d6522049e48640be058b31afac");
-            var httpClient = _httpClientFactory.CreateClient();
-            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {
-                return true;
-            }
-            return false;
+            throw new NotImplementedException();
         }
     }
 }
